107b480e46be83f5c421b249843d8d940d8efe84
ahnaf4d <ahnaftahmid802@gmail.com>
2024-09-26 15:13:03 +0600

    GCD loop problem added

---
+++ general-question/9.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#define endl "\n"
+using namespace std;
+int GCD(int a, int b)
+{
+    while (a % b != 0)
+    {
+        int quotient = a / b;
+        int reminder = a % b;
+        a = b;
+        b = reminder;
+    }
+    return b;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (b == 0)
+    {
+        cout << a << endl;
+    }
+    else if (a == 0)
+    {
+        cout << b << endl;
+    }
+    else if (a > b)
+    {
+        int gcdVal = GCD(a, b);
+        cout << gcdVal << endl;
+    }
+    else
+    {
+        int gcdVal = GCD(b, a);
+        cout << gcdVal << endl;
+    }
+    return 0;
+}
